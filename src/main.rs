3d3c4d70b3ebdddbcffd5c05ddbb518e39123ee9
PikminGuts92 <PikminGuts92@gmail.com>
2020-05-09 23:15:54 -0400

    Created basic gui window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use iced::{
+    button, scrollable, slider, text_input, Align, Button, Checkbox, Column,
+    Container, Element, Length, ProgressBar, Radio, Row, Sandbox, Scrollable,
+    Settings, Slider, Space, Text, TextInput,
+};
+
+#[derive(Default)]
+struct Styling {
+    //theme: style::Theme,
+    scroll: scrollable::State,
+    input: text_input::State,
+    input_value: String,
+    button: button::State,
+    slider: slider::State,
+    slider_value: f32,
+    toggle_value: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Styling {
+    type Message = Message; // type Message = ();
+
+    fn new() -> Self {
+        Styling::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Default Window")
+    }
+
+    fn update(&mut self, _message: Message) {
+        // This application has no interactions
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(
+                Text::new("Hello, world!")
+            )
+            .push(
+                Button::new(&mut self.button, Text::new("Click Me!"))
+                    .on_press(Message::DecrementPressed)
+                    //.on_press(Message::DecrementPressed),
+            )
+            .into()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let settings = Settings::default();
+
+    Styling::run(settings);
 }
