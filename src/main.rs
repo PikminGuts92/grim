9869c9dc8b333ad51545d51539eacc8ebff49184
PikminGuts92 <PikminGuts92@gmail.com>
2020-05-27 23:27:29 -0400

    Added printing of milo entries

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(with_options)] // TODO: Move to seperate lib file
+use std::cmp::Ordering;
 use std::env;
 use std::path::Path;
 use thiserror::Error;
 
 mod grim;
 use grim::io::*;
+use grim::scene::{Object, ObjectDir};
 
 #[derive(Error, Debug)]
 pub enum ArgError {
@@ -33,7 +35,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut reader: Box<dyn Stream> = Box::new(FileStream::new(file_path)?);
     let milo = MiloArchive::from_stream(&mut reader)?;
 
-    let obj_dir = milo.unpack_directory()?;
+    let mut obj_dir = milo.unpack_directory()?;
+
+    obj_dir.entries.sort_by(compare_entries);
+    output_entries(&obj_dir);
 
     Ok(())
+}
+
+fn compare_entries(a : &grim::scene::Object, b: &grim::scene::Object) -> Ordering {
+    // Unpack entries
+    let a = match a {
+        Object::Packed(obj) => obj
+    };
+
+    let b = match b {
+        Object::Packed(obj) => obj
+    };
+
+    // Compare type then name
+    match a.object_type.cmp(&b.object_type) {
+        Ordering::Less => Ordering::Less,
+        Ordering::Greater => Ordering::Greater,
+        Ordering::Equal => a.name.cmp(&b.name)
+    }
+}
+
+fn output_entries(milo_dir: &ObjectDir) {
+    let mut obj_type: &str = &String::new();
+
+    for obj in milo_dir.entries.iter() {
+        let entry = match obj {
+            Object::Packed(packed) => packed
+        };
+
+        // Print type if first in collection or new type
+        if obj_type != &entry.object_type {
+            println!("{}", &entry.object_type);
+        }
+
+        obj_type = &entry.object_type;
+
+        println!("\t{}", &entry.name);
+    }
 }
