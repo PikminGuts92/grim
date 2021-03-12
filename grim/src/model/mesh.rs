82e0f7ddaa0f6dfd6bbc22b0641b8f49a6f993e6
PikminGuts92 <PikminGuts92@gmail.com>
2021-03-12 14:55:19 -0500

    Regenerate cargo lock

--- grim/src/model/mesh.rs
+++ grim/src/model/mesh.rs
@@ -1,6 +1,9 @@
 use gltf::{Gltf, Scene};
 use std::error::Error;
+use std::path::Path;
 
 pub fn open_model<T>(model_path: T) -> Result<(), Box<dyn Error>> where T: AsRef<Path>  {
-    let model = Gltf::open(path)?;
+    let model = Gltf::open(model_path)?;
+
+    Ok(())
 }
