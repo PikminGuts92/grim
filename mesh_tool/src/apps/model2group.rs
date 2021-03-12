74e5d61ed0cd797e204f709ce8aa1742533fc23f
PikminGuts92 <PikminGuts92@gmail.com>
2021-03-11 20:58:41 -0500

    Stub mesh tool project

---
+++ mesh_tool/src/apps/model2group.rs
@@ -0,0 +1,34 @@
+use crate::apps::{SubApp};
+use clap::{Clap};
+
+use std::error::Error;
+
+use std::path::{Path};
+
+
+use grim::{Platform, SystemInfo};
+use grim::io::*;
+use grim::scene::{Object, ObjectDir, PackedObject, Tex};
+
+
+#[derive(Clap, Debug)]
+pub struct Model2GroupApp {
+    #[clap(about = "Path to input model file", required = true)]
+    pub model_path: String,
+    #[clap(about = "Path to output directory", required = true)]
+    pub output_path: String,
+}
+
+// TODO: Get from args
+const SYSTEM_INFO: SystemInfo = SystemInfo {
+    version: 25,
+    platform: Platform::PS3,
+    endian: IOEndian::Big,
+};
+
+impl SubApp for Model2GroupApp {
+    fn process(&mut self) -> Result<(), Box<dyn Error>> {
+
+        Ok(())
+    }
+}
