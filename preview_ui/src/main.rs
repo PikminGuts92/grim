c395ae5435f3229dd8fac032990cb6943b0e025e
PikminGuts92 <PikminGuts92@gmail.com>
2021-04-06 02:02:07 -0400

    Add menu

--- preview_ui/src/main.rs
+++ preview_ui/src/main.rs
@@ -22,9 +22,44 @@ fn main() {
         .run();
 }
 
-fn ui_example(mut egui_context: ResMut<EguiContext>) {
+fn ui_example(mut egui_context: ResMut<EguiContext>, mut app_exit_events: ResMut<Events<bevy::app::AppExit>>) {
     let ctx = &mut egui_context.ctx;
+
+    // Toolbar
+    egui::TopPanel::top("top_panel").show(ctx, |ui| {
+        // ui.heading("Main");
+
+        egui::menu::bar(ui, |ui| {
+            egui::menu::menu(ui, "File", |ui| {
+                ui.set_min_width(80.0);
+
+                ui.button("Open");
+                ui.separator();
+                if ui.button("Exit").clicked() {
+                    // Close app
+                    app_exit_events.send(bevy::app::AppExit);
+                }
+            });
+
+        });
+    });
+
+    // Side panel
+    /* egui::SidePanel::left("side_panel", 500.0).show(ctx, |ui| {
+        ui.set_min_width(400.0);
+        ui.heading("Options");
+    });*/
+
+    // Hide menu shadow
+    let mut style: egui::Style = (*ctx.style()).clone();
+    let shadow_color = style.visuals.window_shadow.color.clone();
+    style.visuals.window_shadow.color = shadow_color.linear_multiply(0.0);
+    ctx.set_style(style);
+
     egui::Window::new("Hello").show(ctx, |ui| {
+        // let mut style = ui.style_mut();
+        // style.visuals.code_bg_color = style.visuals.code_bg_color.linear_multiply(0.1);
+
         ui.label("world");
     });
 }
