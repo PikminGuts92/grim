21327a156052aec33297c1342b84aea43d10a45a
PikminGuts92 <PikminGuts92@gmail.com>
2021-04-10 12:39:40 -0400

    Update to bevy 5

--- preview_ui/src/main.rs
+++ preview_ui/src/main.rs
@@ -2,13 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::{prelude::*, render::camera::PerspectiveProjection};
-use bevy_4x_camera::{CameraRig, CameraRigBundle, FourXCameraPlugin};
+// use bevy_4x_camera::{CameraRig, CameraRigBundle, FourXCameraPlugin};
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 fn main() {
     App::build()
-        .add_resource(Msaa { samples: 8 })
-        .add_resource(WindowDescriptor {
+        .insert_resource(Msaa { samples: 8 })
+        .insert_resource(WindowDescriptor {
             title: String::from("Preview"),
             width: 1920.0,
             height: 1080.0,
@@ -18,15 +18,15 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
-        .add_plugin(FourXCameraPlugin)
+        // .add_plugin(FourXCameraPlugin)
         .add_system(ui_example.system())
-        .add_system(data_ui.system())
+        // .add_system(data_ui.system())
         .add_startup_system(setup.system())
         .run();
 }
 
-fn ui_example(mut egui_context: ResMut<EguiContext>, mut app_exit_events: ResMut<Events<bevy::app::AppExit>>) {
-    let ctx = &mut egui_context.ctx;
+fn ui_example(mut egui_ctx: ResMut<EguiContext>, mut event_writer: EventWriter<bevy::app::AppExit>) {
+    let ctx = &mut egui_ctx.ctx();
 
     // Toolbar
     egui::TopPanel::top("top_panel").show(ctx, |ui| {
@@ -46,7 +46,7 @@ fn ui_example(mut egui_context: ResMut<EguiContext>, mut app_exit_events: ResMut
                 ui.separator();
                 if ui.button("Exit").clicked() {
                     // Close app
-                    app_exit_events.send(bevy::app::AppExit);
+                    event_writer.send(bevy::app::AppExit);
                 }
             });
 
@@ -88,7 +88,7 @@ fn ui_example(mut egui_context: ResMut<EguiContext>, mut app_exit_events: ResMut
     style.visuals.window_shadow.color = shadow_color.linear_multiply(0.0);
     ctx.set_style(style);
 
-    egui::Window::new("Hello").show(ctx, |ui| {
+    egui::Window::new("Hello 2").show(ctx, |ui| {
         // let mut style = ui.style_mut();
         // style.visuals.code_bg_color = style.visuals.code_bg_color.linear_multiply(0.1);
 
@@ -97,30 +97,36 @@ fn ui_example(mut egui_context: ResMut<EguiContext>, mut app_exit_events: ResMut
 }
 
 fn setup(
-    commands: &mut Commands,
+    mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // add entities to the world
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    /*// add entities to the world
     commands
-        // plane
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-            ..Default::default()
-        })
-        // cube
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-            ..Default::default()
-        })
-        // light
-        .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-            ..Default::default()
-        })
         // camera w/ plugin rig
         .spawn(CameraRigBundle::default())
         .with_children(|cb| {
@@ -129,14 +135,14 @@ fn setup(
                     .looking_at(Vec3::default(), Vec3::unit_y()),
                 ..Default::default()
             });
-        });
+        });*/
 }
 
-fn data_ui(
-    egui: Res<bevy_egui::EguiContext>,
+/*fn data_ui(
+    egui_ctx: Res<bevy_egui::EguiContext>,
     mut cameras: Query<&mut CameraRig>,
 ) {
     for mut c in cameras.iter_mut() {
-        c.disable = egui.ctx.wants_pointer_input();
+        c.disable = egui_ctx.ctx().wants_pointer_input();
     }
-}
+}*/
