a3502572d122aefe8205ee0f6fd83bc7de1d0510
PikminGuts92 <PikminGuts92@gmail.com>
2021-07-07 00:28:40 -0400

    Update meta

--- grim/src/scene/meta.rs
+++ grim/src/scene/meta.rs
@@ -4,7 +4,18 @@ pub trait Version {
     fn set_version(&mut self, version: Option<u32>);
 }
 
-pub struct Metadata {
+pub trait Name {
+    fn get_name(&self) -> String;
+    fn set_name(&mut self, name: String);
+}
+
+pub trait AsObject<T>: AsMut<T> + AsRef<T> {}
+
+pub trait Metadata: AsObject<MetadataObject> {
+    //fn get_metadata(&mut)
+}
+
+pub struct MetadataObject {
     pub version: Option<u32>,
     pub revision: Option<u32>,
     pub r#type: String,
