eab674943a1b4c8f95292551961289e57f5458de
PikminGuts92 <PikminGuts92@gmail.com>
2021-07-29 23:57:18 -0400

    Add mesh trait

--- grim_traits/src/lib.rs
+++ grim_traits/src/lib.rs
@@ -34,6 +34,74 @@ pub trait Draw {
     fn set_draw_order(&mut self, draw_order: f32);
 }
 
+pub struct Color4 {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+pub struct BoneTrans {
+    pub name: String,
+    pub trans: Matrix,
+}
+
+pub struct UV {
+    pub u: f32,
+    pub v: f32,
+}
+
+pub struct Vector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+pub struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+pub struct Vector4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+pub struct Vertex {
+    pub pos: Vector4,
+    pub normals: Vector4,
+    pub color: Color4,
+    pub uv: UV,
+}
+
+pub trait Mesh : Draw + Trans {
+    fn get_material(&self) -> &String;
+    fn get_material_mut(&mut self) -> &mut String;
+    fn set_material(&mut self, mat: String);
+
+    fn get_mesh_name(&self) -> &String;
+    fn get_mesh_name_mut(&mut self) -> &mut String;
+    fn set_mesh_name(&mut self, mesh: String);
+
+    fn get_vertices(&self) -> &Vec<Vertex>;
+    fn get_vertices_mut(&mut self) -> &mut Vec<Vertex>;
+    fn set_vertices(&mut self, vertices: Vec<Vertex>);
+
+    fn get_faces(&self) -> &Vec<[u16; 3]>;
+    fn get_faces_mut(&mut self) -> &mut Vec<[u16; 3]>;
+    fn set_faces(&mut self, faces: Vec<[u16; 3]>);
+
+    fn get_face_groups(&self) -> &Vec<u8>;
+    fn get_face_groups_mut(&mut self) -> &mut Vec<u8>;
+    fn set_face_groups(&mut self, face_groups: Vec<u8>);
+
+    fn get_bones(&self) -> &Vec<BoneTrans>;
+    fn get_bones_mut(&mut self) -> &mut Vec<BoneTrans>;
+    fn set_bones(&mut self, bones: Vec<BoneTrans>);
+}
+
 pub trait Poll {
     fn get_target_1(&self) -> &String;
     fn get_target_1_mut(&mut self) -> &mut String;
