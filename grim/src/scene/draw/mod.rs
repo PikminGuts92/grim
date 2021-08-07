17570222882b5547f9f0b7416b5b348f54ddd096
PikminGuts92 <PikminGuts92@gmail.com>
2021-08-06 23:32:59 -0400

    Clean up draw struct

--- grim/src/scene/draw/mod.rs
+++ grim/src/scene/draw/mod.rs
@@ -1,12 +1,8 @@
 use grim_macros::*;
 use grim_traits::scene::{Draw, Mesh, Trans};
 
-//#[derive(Version)]
 #[milo(Draw)]
-#[milo_super(Mesh, Trans)]
-pub struct DrawObject {
-    //pub version: Option<u32>,
-}
+pub struct DrawObject;
 
 
 impl DrawObject {
