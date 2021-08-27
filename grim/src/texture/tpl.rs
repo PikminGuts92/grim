6072e71fa3c1b3e6290f33180d74005e3a41558e
PikminGuts92 <PikminGuts92@gmail.com>
2021-08-27 01:51:37 -0400

    Fix wii texture decoding

--- grim/src/texture/tpl.rs
+++ grim/src/texture/tpl.rs
@@ -35,8 +35,8 @@ fn decode_cmp_image(tpl_img: &[u8], rgba: &mut [u8], width: u32) {
     let bpp = get_tpl_bpp(&TPLEncoding::RGBA8);
 
     // Get block counts
-    let block_x = width >> 2;
-    let block_y = calculate_texture_height(tpl_img.len(), width, bpp) >> 2;
+    let block_x = width >> 3;
+    let block_y = calculate_texture_height(tpl_img.len(), width, bpp) >> 3;
     let block_size = ((16 * bpp) / 8) as usize;
 
     let mut packed_0;
@@ -55,41 +55,44 @@ fn decode_cmp_image(tpl_img: &[u8], rgba: &mut [u8], width: u32) {
 
     for by in 0..block_y {
         for bx in 0..block_x {
-            if (bx >> 1) & 1 == 1 {
-                x = by << 1;
-                y = (bx ^ 1) << 1;
-            } else {
-                x = bx << 2;
-                y = by << 2;
+            for t in 0..4 {
+                // TODO: Probably refactor
+                // Hacky way of adding 2x2 groups
+                x = (bx << 3) + ((t % 2) << 2);
+                y = by << 3;
+
+                if t >= 2 {
+                    y += 4;
+                }
+
+                // Read packed bytes (Wii is reverse endian compared to DXT)
+                packed_0 = read_as_u16_be(&tpl_img[i..(i + 2)]);
+                packed_1 = read_as_u16_be(&tpl_img[(i + 2)..(i + 4)]);
+
+                // Unpack colors to rgba
+                unpack_rgb565(packed_0, &mut color_0);
+                unpack_rgb565(packed_1, &mut color_1);
+
+                // Interpolate other colors
+                if packed_0 > packed_1 {
+                    // 4 colors
+                    mix_colors_66_33(&color_0, &color_1, &mut color_2);
+                    mix_colors_66_33(&color_1, &color_0, &mut color_3);
+                } else {
+                    // 3 colors + transparent
+                    mix_colors_50_50(&color_0, &color_1, &mut color_2);
+                    zero_out(&mut color_3);
+                }
+
+                // Unpack color indicies
+                unpack_indices_be(&tpl_img[(i + 4)..(i + 8)], &mut indices);
+
+                // Copy colors to pixel data
+                let colors = [&color_0, &color_1, &color_2, &color_3];
+                copy_unpacked_pixels(rgba, &colors, &indices, x, y, width);
+
+                i += block_size;
             }
-
-            // Read packed bytes (Wii is reverse endian compared to DXT)
-            packed_0 = read_as_u16_be(&tpl_img[i..(i + 2)]);
-            packed_1 = read_as_u16_be(&tpl_img[(i + 2)..(i + 4)]);
-
-            // Unpack colors to rgba
-            unpack_rgb565(packed_0, &mut color_0);
-            unpack_rgb565(packed_1, &mut color_1);
-
-            // Interpolate other colors
-            if packed_0 > packed_1 {
-                // 4 colors
-                mix_colors_66_33(&color_0, &color_1, &mut color_2);
-                mix_colors_66_33(&color_1, &color_0, &mut color_3);
-            } else {
-                // 3 colors + transparent
-                mix_colors_50_50(&color_0, &color_1, &mut color_2);
-                zero_out(&mut color_3);
-            }
-
-            // Unpack color indicies
-            unpack_indices_be(&tpl_img[(i + 4)..(i + 8)], &mut indices);
-
-            // Copy colors to pixel data
-            let colors = [&color_0, &color_1, &color_2, &color_3];
-            copy_unpacked_pixels(rgba, &colors, &indices, x, y, width);
-
-            i += block_size;
         }
     }
 }
