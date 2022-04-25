d79c3bf37e719c915cfc421cc6b22c1b6e60ded8
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-04-24 22:09:54 -0400

    Update clap in p9 project

--- p9_scene_tool/src/apps/milo2midi.rs
+++ p9_scene_tool/src/apps/milo2midi.rs
@@ -1,5 +1,5 @@
 use crate::apps::{SubApp};
-use clap::{App, Arg, Clap};
+use clap::Parser;
 use std::error::Error;
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -10,13 +10,13 @@ use grim::io::*;
 use grim::scene::{Object, ObjectDir, PackedObject, Tex};
 use grim::texture::{Bitmap, write_rgba_to_file};
 
-#[derive(Clap, Debug)]
+#[derive(Parser, Debug)]
 pub struct Milo2MidiApp {
-    #[clap(about = "Path to input milo scene", required = true)]
+    #[clap(help = "Path to input milo scene", required = true)]
     pub milo_path: String,
-    #[clap(about = "Path to output MIDI file", required = true)]
+    #[clap(help = "Path to output MIDI file", required = true)]
     pub midi_path: String,
-    #[clap(long, about = "Base MIDI file")]
+    #[clap(long, help = "Base MIDI file")]
     pub base_midi: String
 }
 
--- p9_scene_tool/src/apps/mod.rs
+++ p9_scene_tool/src/apps/mod.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg, Clap};
+use clap::{Parser, Subcommand};
 use std::error::Error;
 
 mod milo2midi;
@@ -12,14 +12,14 @@ pub(crate) trait SubApp {
     fn process(&mut self) -> Result<(), Box<dyn Error>>;
 }
 
-#[derive(Clap, Debug)]
+#[derive(Parser, Debug)]
 #[clap(name = PKG_NAME, version = VERSION, about = "Use this tool for modding scenes from milo engine based games (project 9)")]
 struct Options {
     #[clap(subcommand)]
     commands: SubCommand,
 }
 
-#[derive(Clap, Debug)]
+#[derive(Subcommand, Debug)]
 enum SubCommand {
     #[clap(name = "milo2midi", about = "Creates MIDI from milo scene")]
     Milo2Midi(Milo2MidiApp)
