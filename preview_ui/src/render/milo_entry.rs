1d4670937a7483b0d05abf634fad8ee7c9e3996a
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-05-23 22:43:33 -0400

    User more texture encodings with bevy

--- preview_ui/src/render/milo_entry.rs
+++ preview_ui/src/render/milo_entry.rs
@@ -15,7 +15,7 @@ use grim::scene::{GroupObject, Matrix, MeshObject, Milo, MiloObject, Object, Obj
 use grim::texture::Bitmap;
 
 use crate::WorldMesh;
-use super::{map_matrix, MiloLoader};
+use super::{map_matrix, MiloLoader, TextureEncoding};
 
 pub fn render_milo_entry(
     commands: &mut Commands,
@@ -328,9 +328,9 @@ fn get_product_local_mat<'a>(
     map_matrix(milo_object.get_local_xfm())
 }
 
-fn get_texture<'a, 'b>(loader: &'b mut MiloLoader<'a>, tex_name: &str, system_info: &SystemInfo) -> Option<&'b (&'a Tex, Vec<u8>)> {
+fn get_texture<'a, 'b>(loader: &'b mut MiloLoader<'a>, tex_name: &str, system_info: &SystemInfo) -> Option<&'b (&'a Tex, Vec<u8>, TextureEncoding)> {
     // Check for cached texture
-    if let Some(cached) = loader.get_cached_texture(tex_name) {
+    if let Some(cached) = loader.get_cached_texture(tex_name).take() {
         // TODO: Figure out why commented out line doesn't work (stupid lifetimes)
         //return Some(cached);
         return loader.get_cached_texture(tex_name);
@@ -340,19 +340,50 @@ fn get_texture<'a, 'b>(loader: &'b mut MiloLoader<'a>, tex_name: &str, system_in
     // TODO: Check for external textures
     loader.get_texture(tex_name)
         .and_then(|t| t.bitmap.as_ref())
-        .and_then(|b| b.unpack_rgba(system_info).ok())
-        .and_then(move |rgba| {
+        .and_then(|b| match (system_info.platform, b.encoding) {
+            // TODO: Uncomment when BCn textures are supported
+            /*(Platform::X360 | Platform::PS3, 8 | 24 | 32) => {
+                let enc = match b.encoding {
+                     8 => TextureEncoding::DXT1,
+                    24 => TextureEncoding::DXT5,
+                    32 | _ => TextureEncoding::ATI2,
+                };
+
+                let mut data = b.raw_data.to_owned();
+
+                if system_info.platform.eq(&Platform::X360) {
+                    // Swap bytes
+                    for ab in data.chunks_mut(2) {
+                        let tmp = ab[1];
+
+                        ab[0] = ab[1];
+                        ab[1] = tmp;
+                    }
+                }
+
+                Some((data, enc))
+            },*/
+            _ => b.unpack_rgba(system_info).ok()
+                .and_then(|rgba| Some((rgba, TextureEncoding::RGBA)))
+        })
+        .and_then(move |(rgba, enc)| {
             // Cache decoded texture
-            loader.set_cached_texture(tex_name, rgba);
+            loader.set_cached_texture(tex_name, rgba, enc);
             loader.get_cached_texture(tex_name)
         })
 }
 
-fn map_texture<'a>(tex: &'a (&'a Tex, Vec<u8>)) -> Image {
-    let (bitmap, rgba) = tex;
+fn map_texture<'a>(tex: &'a (&'a Tex, Vec<u8>, TextureEncoding)) -> Image {
+    let (bitmap, rgba, enc) = tex;
+
+    let bpp: usize = match enc {
+        TextureEncoding::DXT1 => 4,
+        TextureEncoding::DXT5 | TextureEncoding::ATI2 => 8,
+        TextureEncoding::RGBA => 32,
+    };
 
     // TODO: Figure out how bevy can support mip maps
-    let tex_size = (bitmap.width as usize) * (bitmap.height as usize) * 4;
+    let tex_size = ((bitmap.width as usize) * (bitmap.height as usize) * bpp) / 8;
 
     let mut texture = Image::new_fill(
         Extent3d {
@@ -362,7 +393,12 @@ fn map_texture<'a>(tex: &'a (&'a Tex, Vec<u8>)) -> Image {
         },
         TextureDimension::D2,
         &rgba[..tex_size],
-        TextureFormat::Rgba8UnormSrgb,
+        match enc {
+            TextureEncoding::DXT1 => TextureFormat::Bc1RgbaUnorm,
+            TextureEncoding::DXT5 => TextureFormat::Bc3RgbaUnorm,
+            TextureEncoding::ATI2 => TextureFormat::Bc5RgUnorm,
+            _ => TextureFormat::Rgba8UnormSrgb,
+        }
     );
 
     // Update texture wrap mode
