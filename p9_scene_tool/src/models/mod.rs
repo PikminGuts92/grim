188eef8bb4da48946e4265125f6b9d7c595b1969
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-08-07 23:45:02 -0400

    Implement custom deserialization for P9Song

--- p9_scene_tool/src/models/mod.rs
+++ p9_scene_tool/src/models/mod.rs
@@ -1,10 +1,10 @@
 mod gdrb;
 mod tbrb;
+mod serialization;
 
 pub use gdrb::*;
 pub use tbrb::*;
-use serde::{Deserialize, Serialize, Serializer, ser::{SerializeStruct, SerializeSeq}};
-use serde::de::{value::StringDeserializer, Visitor};
+use serde::{Deserialize, Serialize};
 
 #[derive(Default)]
 pub struct P9Song {
@@ -37,38 +37,4 @@ pub struct LyricEvent {
     #[serde(alias = "Pos", rename = "pos")] pub position: [f32; 3],
     #[serde(alias = "Rot", rename = "rot")] pub rotation: [f32; 4],
     #[serde(alias = "Scale")] pub scale: [f32; 3],
-}
-
-impl<'de> Deserialize<'de> for P9Song {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        todo!()
-    }
-}
-
-impl Serialize for P9Song {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        let mut song = serializer.serialize_struct("P9Song", 1)?;
-        let game_format = match self.preferences {
-            SongPreferences::TBRB(_) => "TBRB",
-            SongPreferences::GDRB(_) => "GDRB"
-        };
-
-        song.serialize_field("name", &self.name)?;
-        song.serialize_field("format", game_format)?;
-
-        match &self.preferences {
-            SongPreferences::TBRB(tbrb_prefs) => {
-                song.serialize_field("preferences", tbrb_prefs)?;
-            },
-            SongPreferences::GDRB(gdrb_prefs) => {
-                song.serialize_field("preferences", gdrb_prefs)?;
-            }
-        }
-
-        if let Some(lyric_configs) = &self.lyric_configurations {
-            song.serialize_field("lyric_configs", lyric_configs)?;
-        }
-
-        song.end()
-    }
 }
