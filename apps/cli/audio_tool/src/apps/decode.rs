0c62bcf0afe6c72d8a9f2bfdaff3161fd37e7111
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-02-01 19:35:31 -0500

    Stub synth sample decoder

--- apps/cli/audio_tool/src/apps/decode.rs
+++ apps/cli/audio_tool/src/apps/decode.rs
@@ -1,10 +1,19 @@
 use crate::apps::SubApp;
+use grim::{Platform, SystemInfo};
 use grim::audio::*;
+use grim::io::{FileStream, IOEndian};
+use grim::scene::{ObjectReadWrite, SampleData, SynthSample};
 
 use clap::Parser;
 use std::error::Error;
+use std::io::{Read, Seek};
 use std::fs;
 use std::path::{Path, PathBuf};
+
+enum FileType {
+    Vgs,
+    SynthSample(u32, IOEndian)
+}
 
 #[derive(Parser, Debug)]
 pub struct DecoderApp {
@@ -19,17 +28,34 @@ impl SubApp for DecoderApp {
         let input_path = Path::new(&self.input_path);
         let ouput_path = Path::new(&self.output_path);
 
-        let _input_ext = match input_path.extension().and_then(|e| e.to_str()) {
-            Some(ext) if "vgs".eq_ignore_ascii_case(ext) => ext,
+        let input_type = match input_path.extension().and_then(|e| e.to_str()) {
+            Some(ext) if "vgs".eq_ignore_ascii_case(ext) => FileType::Vgs,
             Some(ext) => {
-                println!("Input audio with extension \".{ext}\" is not supported");
-                return Ok(());
+                // Guess file type from binary structure
+                if let Some(file_type) = guess_type_from_magic(input_path) {
+                    file_type
+                } else {
+                    println!("Input audio with extension \".{ext}\" is not supported");
+                    return Ok(());
+                }
             },
             _ => {
-                println!("Input audio not supported");
-                return Ok(());
+                // Guess file type from binary structure
+                if let Some(file_type) = guess_type_from_magic(input_path) {
+                    file_type
+                } else {
+                    println!("Input audio not supported");
+                    return Ok(());
+                }
             }
         };
+
+        let input_type_name = match input_type {
+            FileType::Vgs => "VGS",
+            FileType::SynthSample(_, _) => "SynthSample",
+        };
+
+        println!("Detected input file type of \"{input_type_name}\"");
 
         let _output_ext = match ouput_path.extension().and_then(|e| e.to_str()) {
             Some(ext) if "wav".eq_ignore_ascii_case(ext) => ext,
@@ -43,25 +69,91 @@ impl SubApp for DecoderApp {
             }
         };
 
-        let mut input_file = std::fs::File::open(input_path)?;
-        let vgs_file = VgsFile::from_reader(&mut input_file)?;
+        // Decode (returns interleaved audio samples)
+        println!("Decoding...");
+        let (sample_data, channels, sample_rate) = match input_type {
+            FileType::Vgs => decode_vgs_file(input_path)?,
+            FileType::SynthSample(version, endian) => {
+                let sys_info = SystemInfo {
+                    version,
+                    platform: Platform::X360, // Doesn't matter here
+                    endian
+                };
 
-        let channel_count = vgs_file.get_channel_count();
-        let sample_rate = vgs_file.get_sample_rate();
-
-        let channels = vgs_file.decode_samples_as_channels();
-        let sample_count = channels.iter().map(|c| c.len()).max().unwrap_or_default();
-        let mut interleaved_data = Vec::new();
-
-        // TODO: Figure out more efficient way to do this
-        for sample_idx in 0..sample_count {
-            for sample in channels.iter().map(|c| c.get(sample_idx).map(|s| *s).unwrap_or_default()) {
-                interleaved_data.push(sample);
-            }
-        }
+                decode_synth_sample_file(input_path, &sys_info)?
+            },
+        };
 
         // Encode to wav
-        let encoder = WavEncoder::new(&interleaved_data, channel_count as u16, sample_rate);
-        encoder.encode_to_file(ouput_path).map_err(|e| Box::new(e) as Box<dyn Error>)
+        let encoder = WavEncoder::new(&sample_data, channels, sample_rate);
+        encoder.encode_to_file(ouput_path).map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+        println!("Wrote output to \"{}\"", ouput_path.to_str().unwrap_or_default());
+        Ok(())
+    }
+}
+
+fn decode_vgs_file(file_path: &Path) -> Result<(Vec<i16>, u16, u32), Box<dyn Error>> {
+    let mut input_file = std::fs::File::open(file_path)?;
+    let vgs_file = VgsFile::from_reader(&mut input_file)?;
+
+    let channel_count = vgs_file.get_channel_count();
+    let sample_rate = vgs_file.get_sample_rate();
+
+    let channels = vgs_file.decode_samples_as_channels();
+    let sample_count = channels.iter().map(|c| c.len()).max().unwrap_or_default();
+    let mut interleaved_data = Vec::new();
+
+    // TODO: Figure out more efficient way to do this
+    for sample_idx in 0..sample_count {
+        for sample in channels.iter().map(|c| c.get(sample_idx).map(|s| *s).unwrap_or_default()) {
+            interleaved_data.push(sample);
+        }
+    }
+
+    Ok((interleaved_data, channel_count as u16, sample_rate))
+}
+
+fn decode_synth_sample_file(file_path: &Path, info: &SystemInfo) -> Result<(Vec<i16>, u16, u32), Box<dyn Error>> {
+    let mut stream = FileStream::from_path_as_read_open(file_path)?;
+
+    // Open synth sample
+    let mut synth_sample = SynthSample::default();
+    synth_sample.load(&mut stream, info)?;
+
+    //Ok((interleaved_data, channel_count as u16, sample_rate))
+    todo!()
+}
+
+fn guess_type_from_magic(file_path: &Path) -> Option<FileType> {
+    // Read first 8 bytes of file
+    let (magic, version) = {
+        let mut magic = [0u8; 4];
+        let mut version = [0u8; 4];
+
+        let mut file = std::fs::File::open(file_path).ok()?;
+        file.read_exact(&mut magic).ok()?;
+        file.read_exact(&mut version).ok()?;
+
+        (magic, version)
+    };
+
+    if magic.eq(b"VgS!") {
+        return Some(FileType::Vgs);
+    }
+
+    let m = i32::from_le_bytes(magic);
+    let v = i32::from_le_bytes(version);
+
+    // First interpret as little endian, then try as big endian
+    guess_from_versions(m, v, IOEndian::Little)
+        .or_else(|| guess_from_versions(m.swap_bytes(), v.swap_bytes(), IOEndian::Big))
+}
+
+fn guess_from_versions(m: i32, v: i32, en: IOEndian) -> Option<FileType> {
+    match (m, v) {
+        (5, 0) => Some(FileType::SynthSample(24, en)),
+        (5, 1 | 2) => Some(FileType::SynthSample(25, en)),
+        _ => None
     }
 }
