7df1ab5cbb0ed6d788fb20b904f1a59b83ba9be3
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-06-05 22:34:04 -0400

    Basic print to dta

--- core/grim/src/dta/mod.rs
+++ core/grim/src/dta/mod.rs
@@ -2,9 +2,28 @@ mod errors;
 mod io;
 mod parser;
 
+use std::todo;
+
 pub use errors::*;
 pub use io::*;
 use parser::*;
+
+#[derive(Debug)]
+pub struct DTAFormat {
+    pub use_quoted_symbols: bool, // Ex: 'shortsongname'
+    pub indent_char: char,
+    pub indent_char_count: u8,
+}
+
+impl Default for DTAFormat {
+    fn default() -> Self {
+        Self {
+            use_quoted_symbols: false,
+            indent_char: ' ',
+            indent_char_count: 3
+        }
+    }
+}
 
 #[derive(Debug, Default, PartialEq)]
 pub struct DataString {
@@ -36,6 +55,18 @@ impl DataString {
 impl From<Vec<u8>> for DataString {
     fn from(data: Vec<u8>) -> DataString {
         DataString::from_vec(data)
+    }
+}
+
+impl From<&[u8]> for DataString {
+    fn from(data: &[u8]) -> DataString {
+        DataString::from_vec(data.to_vec())
+    }
+}
+
+impl From<&str> for DataString {
+    fn from(data: &str) -> DataString {
+        DataString::from_string(data)
     }
 }
 
@@ -155,6 +186,144 @@ impl DataArray {
             _ => None
         }
     }
+
+    pub fn print<T: std::io::Write>(&self, stream: &mut T) -> Result<(), std::io::Error> {
+        self.print_with_format(stream, DTAFormat::default())
+    }
+
+    pub fn print_with_format<T: std::io::Write>(&self, stream: &mut T, format: DTAFormat) -> Result<(), std::io::Error> {
+        self.write_to_stream(stream, &format, 0, true).map(|_| ())
+    }
+
+    fn write_to_stream<T: std::io::Write>(&self, stream: &mut T, format: &DTAFormat, depth: u32, is_vertical: bool) -> Result<(bool, u32), std::io::Error> {
+        use std::io::Write;
+
+        const CHAR_NEWLINE: u8 = b'\n';
+        const CHAR_SPACE: u8 = b' ';
+
+        // TODO: Move to separate function
+        fn write_elements<T: std::io::Write>(stream: &mut T, elements: &Vec<DataArray>, format: &DTAFormat, depth: u32) -> Result<(bool, u32), std::io::Error> {
+            let _only_simple_types = elements.iter().all(|e| e.is_simple_type());
+
+            // TODO: Support vertical write
+
+            // Always write first element without special spacing
+            for element in elements.iter().take(1) {
+                element.write_to_stream(stream, format, depth, false)?;
+            }
+
+            for element in elements.iter().skip(1) {
+                stream.write_all(&[CHAR_SPACE])?;
+                element.write_to_stream(stream, format, depth, false)?;
+            }
+
+            Ok((false, 0))
+        }
+
+        let is_newline = false;
+
+        match self {
+            DataArray::Integer(i) => {
+                write!(stream, "{i}")?;
+            },
+            DataArray::Float(f) => {
+                write!(stream, "{f:?}")?; // At least 1 decimal place...
+            },
+            DataArray::Variable(v) => {
+                stream.write_all(b"$")?;
+                stream.write_all(&v.data)?;
+            },
+            //DataArray::Func(_)        => 0x03,
+            DataArray::Object(o) => {
+                todo!("Support object dta serialization")
+            },
+            DataArray::Symbol(s) => {
+                if s.data.iter().any(|c| c.eq(&CHAR_SPACE)) {
+                    // Write as string if it contains space
+                    stream.write_all(b"\"")?;
+                    stream.write_all(&s.data)?;
+                    stream.write_all(b"\"")?;
+                }
+                else if format.use_quoted_symbols {
+                    stream.write_all(b"\'")?;
+                    stream.write_all(&s.data)?;
+                    stream.write_all(b"\'")?;
+                } else {
+                    stream.write_all(&s.data)?;
+                }
+            },
+            DataArray::KDataUnhandled => {
+                stream.write_all(b"kDataUnhandled")?;
+            },
+            DataArray::IfDef(t) => {
+                stream.write_all(b"#ifdef ")?;
+                stream.write_all(&t.data)?;
+            },
+            DataArray::Else => {
+                stream.write_all(b"#else")?;
+            },
+            DataArray::EndIf => {
+                stream.write_all(b"#endif")?;
+            },
+            DataArray::Array(da) => {
+                stream.write_all(b"(")?;
+                write_elements(stream, da, format, depth)?;
+                stream.write_all(b")")?;
+            },
+            DataArray::Command(da) => {
+                stream.write_all(b"{")?;
+                write_elements(stream, da, format, depth)?;
+                stream.write_all(b"}")?;
+            },
+            DataArray::String(s) => {
+                // TODO: Escape certain characters...
+                stream.write_all(b"\"")?;
+                stream.write_all(&s.data)?;
+                stream.write_all(b"\"")?;
+            },
+            DataArray::Property(da) => {
+                stream.write_all(b"[")?;
+                write_elements(stream, da, format, depth)?;
+                stream.write_all(b"]")?;
+            },
+            DataArray::Define(t) => {
+                stream.write_all(b"#define ")?;
+                stream.write_all(&t.data)?;
+            },
+            DataArray::Include(t) => {
+                stream.write_all(b"#include ")?;
+                stream.write_all(&t.data)?;
+            },
+            DataArray::Merge(t) => {
+                stream.write_all(b"#merge ")?;
+                stream.write_all(&t.data)?;
+            },
+            DataArray::IfNDef(t) => {
+                stream.write_all(b"#ifndef ")?;
+                stream.write_all(&t.data)?;
+            },
+            DataArray::Autorun => {
+                stream.write_all(b"#autorun")?;
+            },
+            DataArray::Undef(t) => {
+                stream.write_all(b"#undef ")?;
+                stream.write_all(&t.data)?;
+            },
+        };
+
+        Ok((is_newline, depth))
+    }
+
+    fn is_simple_type(&self) -> bool {
+        match self {
+            DataArray::Integer(_)
+                | DataArray::Float(_)
+                | DataArray::Variable(_)
+                | DataArray::Symbol(_)
+                | DataArray::KDataUnhandled => true,
+            _ => false
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -165,5 +334,49 @@ pub struct RootData {
 impl RootData {
     pub fn new() -> RootData {
         RootData::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::*;
+    use super::*;
+
+    #[rstest]
+    #[case(DataArray::Integer(500), b"500")]
+    #[case(DataArray::Integer(-42), b"-42")]
+    #[case(DataArray::Float(0.0), b"0.0")]
+    #[case(DataArray::Float(0.3), b"0.3")]
+    #[case(DataArray::Float(0.38), b"0.38")]
+    #[case(DataArray::Float(-0.45), b"-0.45")]
+    #[case(DataArray::Variable("test".into()), b"$test")]
+    // TODO: Test case for func
+    #[case(DataArray::Symbol("test".into()), b"test")]
+    #[case(DataArray::Symbol("lol look at these spaces".into()), b"\"lol look at these spaces\"")]
+    #[case(DataArray::KDataUnhandled, b"kDataUnhandled")]
+    #[case(DataArray::IfDef("something1".into()), b"#ifdef something1")]
+    #[case(DataArray::Else, b"#else")]
+    #[case(DataArray::EndIf, b"#endif")]
+    #[case(DataArray::Array(vec![DataArray::Symbol(DataString::from_string("version")), DataArray::String(DataString::from_string("v123"))]), b"(version \"v123\")")]
+    // TODO: Test case for command
+    #[case(DataArray::String("test".into()), b"\"test\"")]
+    // TODO: Test case for property
+    #[case(DataArray::Define("whatever".into()), b"#define whatever")]
+    #[case(DataArray::Include("something.dta".into()), b"#include something.dta")]
+    #[case(DataArray::Merge("something.dta".into()), b"#merge something.dta")]
+    #[case(DataArray::IfNDef("whatever".into()), b"#ifndef whatever")]
+    #[case(DataArray::Autorun, b"#autorun")]
+    #[case(DataArray::Undef("whatever".into()), b"#undef whatever")]
+    fn print_data_array_test<const N: usize>(#[case] data: DataArray, #[case] expected: &[u8; N]) {
+        use std::io::{BufWriter, Write};
+
+        let mut buffer = BufWriter::new(Vec::new());
+        data.print(&mut buffer).unwrap();
+
+        let expected_str = std::str::from_utf8(expected).unwrap();
+        let buffer_str = std::str::from_utf8(buffer.buffer()).unwrap();
+
+        //assert_eq!(expected, buffer.buffer());
+        assert_eq!(expected_str, buffer_str);
     }
 }
