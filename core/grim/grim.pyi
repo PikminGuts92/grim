2606fc585289d6e80a3561ce85e7b2601f0cc19e
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2024-01-18 08:44:41 -0500

    Add type info for bitmap class

--- core/grim/grim.pyi
+++ core/grim/grim.pyi
@@ -13,3 +13,17 @@ class Ark:
 
     @staticmethod
     def from_file_path(path: str) -> Ark: ...
+
+class Bitmap:
+    bpp: int
+    encoding: int
+    mip_maps: int
+    width: int
+    height: int
+    bpl: int
+    raw_data: list[int]
+
+    @staticmethod
+    def from_file_path(path: str) -> Bitmap: ...
+
+    def save_to_file(path: str) -> None: ...
